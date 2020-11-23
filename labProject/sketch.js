7ad6701acd544056ceac9f9f08c2183cc4ec0769
6R360R7 <gromero@clues.org>
2020-11-23 14:11:11 -0600

    editited README content

--- labProject/sketch.js
+++ labProject/sketch.js
@@ -1,7 +1,15 @@
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-}
-
-function draw() {
-
-}
+	createCanvas(120, 120);
+  }
+  
+  function draw() { 
+	background(220);
+	// head
+	rect(50, 20, 10, 10);
+	rect(20, 30, 70, 60);
+	// eyes
+	ellipse(40, 50, 20);
+	ellipse(70, 50, 20);
+	// mouth
+	rect(40, 70, 30, 10);
+  }
